bf808b20737143c5fc57c9de03e92740a24b8e2b
dima-efremov <joyful.efremov99@gmail.com>
2019-03-11 12:54:32 +0300

    deleted book entity

--- BLL/Services/TokenService.cs
+++ BLL/Services/TokenService.cs
@@ -4,7 +4,6 @@ using System.Text;
 using BLL.AppStart;
 using BLL.IdentityWrappers;
 using BLL.Interfaces;
-using Microsoft.Extensions.Configuration;
 using Microsoft.IdentityModel.Tokens;
 
 namespace BLL.Services
@@ -12,11 +11,9 @@ namespace BLL.Services
     public class TokenService : ITokenService
     {
         private readonly JwtSecurityToken _jwt;
-        private readonly IRoleManager _manager;
 
         public TokenService(ITokenSettings configuration, IRoleManager manager)
         {
-            _manager = manager;
             var configuration1 = configuration;
 
             var issuer = configuration1.TokenIssuer;
@@ -24,14 +21,14 @@ namespace BLL.Services
             var secretKey = configuration1.TokenSecretKey;
             var lifetime = configuration1.TokenLifetime;
 
-            var roleClaims = await _manager.GetRoleClaims()
+            var roleClaims = manager.GetRoleClaims("User").GetAwaiter().GetResult();
 
             var currentTime = DateTime.UtcNow;
             _jwt = new JwtSecurityToken(
                 issuer: issuer,
                 audience: audience,
                 notBefore: currentTime,
-                claims: 
+                claims: roleClaims,
                 expires: currentTime.Add(TimeSpan.FromMinutes(int.Parse(lifetime))),
                 signingCredentials: new SigningCredentials(new SymmetricSecurityKey(
                         Encoding.ASCII.GetBytes(secretKey)),
